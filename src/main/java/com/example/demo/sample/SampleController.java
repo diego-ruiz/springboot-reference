fd81f6a907a43e44664cfee4e825e6adb5dcecda
Diego Ruiz <druiz@MacBook-Pro-2.local>
2021-01-11 22:48:53 -0500

    updates for swagger

--- src/main/java/com/example/demo/sample/SampleController.java
+++ src/main/java/com/example/demo/sample/SampleController.java
@@ -1,10 +1,7 @@
 package com.example.demo.sample;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -18,7 +15,6 @@ public class SampleController {
     }
 
     @GetMapping("/employees")
-
     List<Employee> all() {
         return repository.findAll();
     }
