8d8c9e43334133b23c0367d694033660cbebadf4
Diego Ruiz <druiz@MacBook-Pro-2.local>
2021-01-11 22:04:36 -0500

    initial commit

---
+++ src/main/java/com/example/demo/sample/SampleController.java
@@ -0,0 +1,30 @@
+package com.example.demo.sample;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class SampleController {
+
+    private final EmployeeRepository repository;
+
+    SampleController(EmployeeRepository repository) {
+        this.repository = repository;
+    }
+
+    @GetMapping("/employees")
+
+    List<Employee> all() {
+        return repository.findAll();
+    }
+
+    @PostMapping("/employees")
+    Employee newEmployee(@RequestBody Employee newEmployee) {
+        return repository.save(newEmployee);
+    }
+}
